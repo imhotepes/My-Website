e73aa9d15976954c43e26099b14aefe41c40be20
ZulfahmiFajri <6931856cg@gmail.com>
2025-03-16 09:27:12 +0700

    edit config

--- config.php
+++ config.php
@@ -1,15 +1,9 @@
 <?php
-$host = getenv('MYSQLHOST'); // Host database
-$user = getenv('MYSQLUSER'); // User database
-$pass = getenv('MYSQLPASSWORD'); // Password database
-$dbname = getenv('MYSQLDATABASE'); // Nama database
-$port = getenv('MYSQLPORT'); // Port database (3306)
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-$conn = new mysqli($host, $user, $pass, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-} else {
-    echo "Koneksi berhasil ke database!";
-}
+echo "MYSQLHOST: " . getenv("MYSQLHOST") . "<br>";
+echo "MYSQLUSER: " . getenv("MYSQLUSER") . "<br>";
+echo "MYSQLDATABASE: " . getenv("MYSQLDATABASE") . "<br>";
+echo "MYSQLPORT: " . getenv("MYSQLPORT") . "<br>";
 ?>
