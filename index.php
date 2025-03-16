9adf3200bfd8cfd1a58e297400c996f00107323a
ZulfahmiFajri <6931856cg@gmail.com>
2025-03-16 17:09:05 +0700

    edit index.php

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["long_url"])) {
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="width=device-width, initial-scale=1.0">
     <title>URL Shortener</title>
     <style>
         body {
